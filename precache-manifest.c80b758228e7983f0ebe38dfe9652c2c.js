de8f9bc944fc5723b73cea4f9b08a606eb73deef
alanta <alanta.858@gmail.com>
2021-12-11 02:08:09 -0800

    Updates

---
+++ precache-manifest.c80b758228e7983f0ebe38dfe9652c2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9471042c124ff0d9d2f",
+    "url": "/static/css/main.cf77d43f.chunk.css"
+  },
+  {
+    "revision": "d9471042c124ff0d9d2f",
+    "url": "/static/js/main.aa2cc61c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fb799cb7fc135d431452",
+    "url": "/static/js/2.1a7b1086.chunk.js"
+  },
+  {
+    "revision": "d7b3097318d65262f91a0fa054cba380",
+    "url": "/index.html"
+  }
+];
